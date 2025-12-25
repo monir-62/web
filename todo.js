0eb7b4b0b5b360989cb8e6e4b912b76570db6f15
monir-62 <mdmoniruzzamanm62@gmail.com>
2025-12-25 10:08:07 +0600

    Update todo.js

--- todo.js
+++ todo.js
@@ -24,3 +24,11 @@ while(true){
     }
     req=prompt("Enter what to do");
 }
+
+2.for(let i=arr.length-1;i>=0;i--){//to elemenate all the occurances of num
+    for(arr[i]==num){
+        arr.splice(i,1);//for splice thing backward loop is the best ,though i: 0 upto one step ahead of end can also work too
+    }
+}
+
+3.num=Math.floor(num/10);//to avoid decimal floating count unexpected times of number, satisfies num>0 properly
